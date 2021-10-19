177ff8950a9221539ffc33a048fda131a52eedfc
iniyanv <78460090+iniyanv@users.noreply.github.com>
2021-10-19 12:37:21 +0530

    update praser.py

--- praser.py
+++ praser.py
@@ -1,21 +1,7 @@
-#import csv
-#import random
 import requests
-
-#joke = []
-#with open("shortjokes.csv") as csv_file:
-#	jokes = csv.reader(csv_file)
-#	next(jokes)
-#	for line in jokes:
-#		joke.append(line[1])
-
 def text():
-  joker = requests.get('https://icanhazdadjoke.com/slack').json()
-  jk = joker["attachments"][0]
-  return jk["text"]
+    joker = requests.get('https://icanhazdadjoke.com/slack').json()
+    jk = joker["attachments"][0]
+    return jk["text"]
 if __name__ == "__main__":
     print(text())
-#print(text())
-	
-	
-
