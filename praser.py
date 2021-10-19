f7b0722b2d88c56498414110a8d3afc54a0be684
iniyanv <78460090+iniyanv@users.noreply.github.com>
2021-10-19 12:35:48 +0530

    update praser.py

--- praser.py
+++ praser.py
@@ -13,7 +13,8 @@ def text():
   joker = requests.get('https://icanhazdadjoke.com/slack').json()
   jk = joker["attachments"][0]
   return jk["text"]
-  
+if __name__ == "__main__":
+    print(text())
 #print(text())
 	
 	
